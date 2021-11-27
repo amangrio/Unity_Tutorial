cde9c9bf220beea8574373ab757d9e27354b619a
Aris Mangriotis <arismangriotis@Mac-mini-de-Aris.local>
2021-11-27 13:48:34 +0100

    initial commit

---
+++ Unity Project Tutorial/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+	public PlayerMovement movement;
+
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+		if (collisionInfo.collider.tag == "Obstacle") {
+        	movement.enabled = false;
+			FindObjectOfType<GameManager>();
+		}
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
---
+++ Unity Project Tutorial/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewayForce = 500f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+    }
+
+    void FixedUpdate()
+    {
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("q"))
+        {
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
